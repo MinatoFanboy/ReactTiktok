6ba29f523af0a876e1923c224c8ad897902e8927
MinatoFanboy <thanh31299@gmail.com>
2022-05-08 20:40:19 +0700

    UI header part 2

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,23 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img className={cx('avatar')} src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/cf6d63edcee4ce81432e92c4dadffc65~c5_100x100.jpeg?x-expires=1652180400&x-signature=SwAaGvojHxm%2FOVjKbJDC9BNufl0%3D" alt="" />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyễn Văn A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
