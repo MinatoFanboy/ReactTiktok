03d5bd36fbc642aed081b23eb3440865de424369
MinatoFanboy <thanh31299@gmail.com>
2022-05-11 05:58:25 +0700

    Header UI part 3

---
+++ src/components/Button/index.js
@@ -0,0 +1,64 @@
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+import styles from './Button.module.scss';
+
+const cx = classNames.bind(styles);
+
+function Button({
+    to,
+    href,
+    primary = false,
+    outline = false,
+    rounded = false,
+    passProps,
+    small = false,
+    large = false,
+    text = false,
+    leftIcon = false,
+    rightIcon = false,
+    children,
+    onClick,
+    disabled = false,
+}) {
+    let Comp = 'button';
+    const props = {
+        onClick,
+        ...passProps,
+    };
+
+    if (disabled) {
+        Object.keys(props).forEach(key => {
+            if (key.startsWith('on') && typeof props[key] === 'function') {
+                delete props[key];
+            }
+        })
+    }
+
+    if (to) {
+        props.to = to;
+        Comp = Link;
+    } else if (href) {
+        props.href = href;
+        Comp = 'a';
+    }
+
+    const classes = cx('wrapper', {
+        primary,
+        outline,
+        rounded,
+        small,
+        large,
+        text,
+        disabled,
+    });
+
+    return (
+        <Comp className={classes} {...props}>
+            {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
+            <span className={cx('title')}>{children}</span>
+            {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
+        </Comp>
+    );
+}
+
+export default Button;
